fa58a4b6c20b51baaacbe1d4a222d916583d86aa
Draxler2811 <ol1964759@gmail.com>
2024-05-11 13:29:57 -0600

    Ya muestra los nombres y url de mi tabla y ya agrega y oculta el formulario solo falta diseño

--- src/app/components/app-pokemones/pokemones/pokemones.component.ts
+++ src/app/components/app-pokemones/pokemones/pokemones.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderComponent } from "../../app-header/header/header.component";
 import { FooterComponent } from "../../app-footer/footer/footer.component";
 import { ObtenerDatosService } from '../../../service/obtener-datos.service';
 import { NgModule } from '@angular/core';
+import { Pokemon } from '../../../entities/pokemon';
+import { FormBuilder, FormControl, FormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-pokemones',
     standalone: true,
     templateUrl: './pokemones.component.html',
     styleUrl: './pokemones.component.css',
-    imports: [HeaderComponent, FooterComponent]
+    imports: [HeaderComponent, FooterComponent,FormsModule,CommonModule,MatTableModule,ReactiveFormsModule]
 })
-export class PokemonesComponent {
+export class PokemonesComponent implements OnInit {
+  displayedColumns: string[] = ['Nombre','URL'];
   valores:any[]=[];
+  pokemon:Pokemon = new Pokemon();
+  checkoutForm : any;
+  mostrarFormulario: boolean = false;
+  constructor(private   router: Router,private obtenerDatosService : ObtenerDatosService,
+    private formBuilder: FormBuilder,
 
-  constructor(private   router: Router,private obtenerDatosService : ObtenerDatosService) {}
+  ) {
+
+  this.checkoutForm = this.formBuilder.group({
+    name:new FormControl(null,[Validators.minLength(1)]),
+    url: new FormControl(null,[Validators.maxLength(2)]),
+  });
+  }
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+  
   getPokemon() : void{
     this.obtenerDatosService.getPokemon('https://pokeapi.co/api/v2/pokemon').subscribe(
       (items :any) =>{
@@ -24,4 +45,23 @@ export class PokemonesComponent {
       }
     )
   }
+  onSubmit(valorFormulario:any){
+    if((this.checkoutForm.invalid) || (this.checkoutForm.get('name').value == 'angular')){
+      alert('Formulario incorrecto')
+    }else {
+  this.valores.push(valorFormulario);
+  this.checkoutForm.reset();
+  this.mostrarFormulario = false;
+    }
+  }
+  toggleFormulario() {
+    this.mostrarFormulario = !this.mostrarFormulario; // Cambia el estado de visibilidad del formulario
+  }
+  deleteRow(row: any) {
+    const index = this.valores.indexOf(row);
+    if (index > -1) {
+      this.valores.splice(index, 1);
+    }
+  }
+  
 }
